cec951a7db40317af8c2bfea0ee30c1cc0e77bd8
mutluaydin <mutlumelikaydin1@gmail.com>
2022-09-06 21:44:42 +0200

    TodoController add has been created

--- src/main/java/ch/mutlumelik/todolist/todolist/controller/ToDoListController.java
+++ src/main/java/ch/mutlumelik/todolist/todolist/controller/ToDoListController.java
@@ -2,6 +2,7 @@ package ch.mutlumelik.todolist.todolist.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +10,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 @RestController
-@RequestMapping("/todolist")
+@RequestMapping("/todolist/")
 public class ToDoListController {
 
-    @GetMapping("/")
+    @GetMapping("list/")
     public ResponseEntity getToDoList(){
         return ResponseEntity.ok(Arrays.asList("List"));
     }
-
+    @PostMapping("add/")
+    public ResponseEntity add(){
+        return ResponseEntity.ok().body("The object has been created.");
+    }
 }
