dd0ebac0bcc9eb67939017746ec2b6c0e365fccb
mutluaydin <mutlumelikaydin1@gmail.com>
2022-09-06 21:48:43 +0200

    TodoController delete has been created

--- src/main/java/ch/mutlumelik/todolist/todolist/controller/ToDoListController.java
+++ src/main/java/ch/mutlumelik/todolist/todolist/controller/ToDoListController.java
@@ -1,10 +1,7 @@
 package ch.mutlumelik.todolist.todolist.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,5 +17,9 @@ public class ToDoListController {
     @PostMapping("add/")
     public ResponseEntity add(){
         return ResponseEntity.ok().body("The object has been created.");
+    }
+    @DeleteMapping("delete/")
+    public ResponseEntity delete(){
+        return ResponseEntity.ok().body("the object has been deleted");
     }
 }
